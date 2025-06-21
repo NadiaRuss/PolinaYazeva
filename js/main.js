8cd0e1e54a9e4ff86ddcddcc32bc38ff4224c03a
Надежда Яковлева <i160220@mail.ru>
2025-06-21 18:05:18 +0700

    добавлен js care

--- js/main.js
+++ js/main.js
@@ -63,3 +63,28 @@ shapeButton.addEventListener('click', () => {
 })
 
 // updateList(faceList, faceData);
+
+        document.addEventListener('DOMContentLoaded', function() {
+            const buttons = document.querySelectorAll('.care__btns__btn');
+            const priceContents = document.querySelectorAll('.care__list');
+            
+            buttons.forEach(button => {
+                button.addEventListener('click', function() {
+                    // Удаляем active у всех кнопок
+                    buttons.forEach(btn => btn.classList.remove('light__btn_active'));
+                    // Добавляем active текущей кнопке
+                    this.classList.add('light__btn_active');
+                    
+                    // Получаем target из data-атрибута
+                    const targetId = this.dataset.target;
+                    
+                    // Скрываем все прайсы
+                    priceContents.forEach(content => {
+                        content.style.display = 'none';
+                    });
+                    
+                    // Показываем выбранный прайс
+                    document.getElementById(targetId).style.display = 'grid';
+                });
+            });
+        });
